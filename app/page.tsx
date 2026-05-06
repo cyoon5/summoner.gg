fb3ba51643073909322a0b0f4c65372c40750091
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-05-06 15:30:34 -0700

    nextJS setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-black-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">summoner.gg</h1>
+      <p className="text-white-400 mt-2">
+        League of Legends analytics platform
+      </p>
+    </div>
+  );
+}
