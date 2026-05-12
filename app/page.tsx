a2b70c9d29903661c09d85862a27f7057a7a6504
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-05-12 11:52:11 -0700

    basic frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-export default function Page() {
+import styles from "./page.module.css"
+
+
+export default async function Page() {
+
+  let api_key = process.env.RIOT_API_KEY;
+  if(!api_key)
+    throw new Error("missing api key");
+
+
+  let link = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/MagicTurtle/KAKA?api_key=${api_key}`
+
+
+
   return (
-    <div className="min-h-screen bg-black-900 text-white flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">summoner.gg</h1>
-      <p className="text-white-400 mt-2">
-        League of Legends analytics platform
-      </p>
+    <div className = {styles.container}>
+      <div>
+          <h1 className = {styles.name}>summoner.gg</h1>
+          <input type = "search" id = {styles.searchbar}></input>
+          <p>
+            League of Legends analytics platform
+          </p>
+      </div>
     </div>
   );
 }
