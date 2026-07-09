1b13d2598feb2d5ad7481fbd13e234eb8a33af0f
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-07-09 11:30:14 -0700

    Searched participant row highlighted in details

--- components/profile/MatchCardDetail/SummonerRow.tsx
+++ components/profile/MatchCardDetail/SummonerRow.tsx
@@ -12,132 +12,134 @@ export default function SummonerRow(prop: SummonerRowProp){
 
     return(
         
-        <div className = {styles.summonerContainer}>
-            
-            
-            <div className = {styles.summoner}>
+        <div className = {prop.searchedParticipant.puuid == prop.participant.puuid? styles.searchedSummonerContainer : ""}>
+
+            <div className = {styles.summonerContainer}>
                 
-                <span className = {styles.champLvlContainer}>
-                    <span className = {styles.champLvl}>{prop.participant.championLevel}</span>
-                </span>
-              
-                <Image
-                    src = {prop.participant.championUrl}
-                    width = {500}
-                    height = {500}
-                    className = {styles.championIcon}
-                    alt = "Champion Icon"
-                />
+                <div className = {styles.summoner}>
 
-                <div className = {styles.spellContainer}>
+                    <span className = {styles.champLvlContainer}>
+                        <span className = {styles.champLvl}>{prop.participant.championLevel}</span>
+                    </span>
+                
                     <Image
-                        src = {prop.participant.summonerSpell1Url}
-                        className = {styles.summonerSpell}
-                        width={500}
-                        height={500}
-                        alt= "Spell Image"
+                        src = {prop.participant.championUrl}
+                        width = {500}
+                        height = {500}
+                        className = {styles.championIcon}
+                        alt = "Champion Icon"
                     />
-                    <Image
-                        src = {prop.participant.summonerSpell2Url}
-                        className = {styles.summonerSpell}
-                        width={500}
-                        height={500}
-                        alt= "Spell Image"
-                    />
-                </div>
 
-                <div className = {styles.runeContainer}>
-                    <div className = {styles.runeSlot}>
+                    <div className = {styles.spellContainer}>
                         <Image
-                            src = {prop.participant.keystoneUrl}
-                            className = {styles.rune}
+                            src = {prop.participant.summonerSpell1Url}
+                            className = {styles.summonerSpell}
                             width={500}
                             height={500}
-                            alt= "Rune Image"
+                            alt= "Spell Image"
                         />
-                    </div>
-                    <div className = {styles.runeSlot}>
                         <Image
-                            src = {prop.participant.secondaryRuneTreeUrl}
-                            className = {styles.secondaryRuneTree}
+                            src = {prop.participant.summonerSpell2Url}
+                            className = {styles.summonerSpell}
                             width={500}
                             height={500}
-                            alt= "Rune Image"
+                            alt= "Spell Image"
                         />
                     </div>
 
-                </div>
-
-                <div className = {styles.nameAndRank}>
-
-                    <Link  
-                        title = {prop.participant.gameName + "#" + prop.participant.tagLine} 
-                        href = {`/profile/na1/${prop.participant.gameName}/${prop.participant.tagLine}`} 
-                        className = {prop.participant.puuid == prop.searchedParticipant.puuid ? styles.searchedParticipant : styles.participant}>{prop.participant.gameName}
-                    </Link>
-
-                    <div className = {styles.rank}>
-                        <Image
-                            src = {`/mini-emblems/diamond.png`}
-                            className = {styles.miniRankedEmblem}
-                            width={500}
-                            height={500}
-                            alt= "Rank Mini Crest"
-                        />
-                        <span> D4 </span>
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div className = {styles.kda}>
-                <span className = {styles.kdaTotal}> {prop.participant.kills}/{prop.participant.deaths}/{prop.participant.assists} </span>
-                <span> {prop.participant.kda + " KDA"} </span>
-            </div>
-
-            <div className = {styles.damage}>
-                <span>{prop.participant.damageDealt.toLocaleString()}</span>
-
-                <div className = {styles.damageBarContainer}>
-                    <div className = {styles.damageBar} style={{ width: `${damageWidth}%` }}></div>
-                </div>
-            </div>
-
-            <div className = {styles.gold}>
-                {prop.participant.totalGoldEarned.toLocaleString()}
-            </div>
-
-            <div className = {styles.creepScore}> 
-                {prop.participant.creepScore}
-            </div>  
-        
-            <div className = {styles.wards}>
-                {prop.participant.visionScore}
-            </div>
-
-            <div className = {styles.itemContainer}>
-                {
-                    prop.participant.itemUrls.map((item:string, i) => (
-                        
-                        <div className = {styles.itemSlot} key = {i}>
-
-                            {
-                                item && <Image
-                                className = {styles.item}
-                                src = {item}
+                    <div className = {styles.runeContainer}>
+                        <div className = {styles.runeSlot}>
+                            <Image
+                                src = {prop.participant.keystoneUrl}
+                                className = {styles.rune}
                                 width={500}
                                 height={500}
-                                alt= "Item Image"
-                                />
-                            }
-
+                                alt= "Rune Image"
+                            />
+                        </div>
+                        <div className = {styles.runeSlot}>
+                            <Image
+                                src = {prop.participant.secondaryRuneTreeUrl}
+                                className = {styles.secondaryRuneTree}
+                                width={500}
+                                height={500}
+                                alt= "Rune Image"
+                            />
                         </div>
 
-                    ))
-                }
+                    </div>
+
+                    <div className = {styles.nameAndRank}>
+
+                        <Link  
+                            title = {prop.participant.gameName + "#" + prop.participant.tagLine} 
+                            href = {`/profile/na1/${prop.participant.gameName}/${prop.participant.tagLine}`} 
+                            className = {prop.participant.puuid == prop.searchedParticipant.puuid ? styles.searchedParticipant : styles.participant}>{prop.participant.gameName}
+                        </Link>
+
+                        <div className = {styles.rank}>
+                            <Image
+                                src = {`/mini-emblems/diamond.png`}
+                                className = {styles.miniRankedEmblem}
+                                width={500}
+                                height={500}
+                                alt= "Rank Mini Crest"
+                            />
+                            <span> D4 </span>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className = {styles.kda}>
+                    <span className = {styles.kdaTotal}> {prop.participant.kills}/{prop.participant.deaths}/{prop.participant.assists} </span>
+                    <span> {prop.participant.kda + " KDA"} </span>
+                </div>
+
+                <div className = {styles.damage}>
+                    <span>{prop.participant.damageDealt.toLocaleString()}</span>
+
+                    <div className = {styles.damageBarContainer}>
+                        <div className = {styles.damageBar} style={{ width: `${damageWidth}%` }}></div>
+                    </div>
+                </div>
+
+                <div className = {styles.gold}>
+                    {prop.participant.totalGoldEarned.toLocaleString()}
+                </div>
+
+                <div className = {styles.creepScore}> 
+                    {prop.participant.creepScore}
+                </div>  
+            
+                <div className = {styles.wards}>
+                    {prop.participant.visionScore}
+                </div>
+
+                <div className = {styles.itemContainer}>
+                    {
+                        prop.participant.itemUrls.map((item:string, i) => (
+                            
+                            <div className = {styles.itemSlot} key = {i}>
+
+                                {
+                                    item && <Image
+                                    className = {styles.item}
+                                    src = {item}
+                                    width={500}
+                                    height={500}
+                                    alt= "Item Image"
+                                    />
+                                }
+
+                            </div>
+
+                        ))
+                    }
+                </div>
+            
             </div>
-        
-        </div>
+        </div>  
     )
 }
