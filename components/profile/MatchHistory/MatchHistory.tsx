49f9ce0b2ab67381740ec056d8a664e334f277d5
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-07-14 09:36:34 -0700

    intersection observer initial setup

--- components/profile/MatchHistory/MatchHistory.tsx
+++ components/profile/MatchHistory/MatchHistory.tsx
@@ -1,9 +1,29 @@
 'use client'
+
 import { MatchHistoryProp } from "@/app/types/match";
 import styles from "./MatchHistory.module.css";
 import MatchCard from "../MatchCard/MatchCard";
+import { useEffect, useRef } from "react";
+
 
 export default function MatchHistory(props: MatchHistoryProp){
+
+    const bottomRef = useRef(null);
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            if(entries[0].isIntersecting) {
+                console.log("bottom seen")
+            }
+        });
+
+        if(bottomRef.current) {
+            observer.observe(bottomRef.current);
+        }
+
+        return () => observer.disconnect(); 
+
+    }, []);
 
 
     return(
@@ -13,21 +33,26 @@ export default function MatchHistory(props: MatchHistoryProp){
             <p className = {styles.matchHeader}> Match History</p>
 
             <div className = {styles.matchHolder}> 
-                {
+            {
 
-                    props.searchedSummoner.map((m, i) => 
-                        m && <MatchCard
-                            key = {m.matchId}
-                            participant = {m}
-                            participants = {props.participantsInMatches[i]}
-                            matchInfo = {props.matchInfoList[i]}
-                        />
-                    )
-                
-                }
+                props.searchedSummoner.map((m, i) => 
+                    m && <MatchCard
+                        key = {m.matchId}
+                        participant = {m}
+                        participants = {props.participantsInMatches[i]}
+                        matchInfo = {props.matchInfoList[i]}
+                    />
+                )
+            
+            }
             </div>
+
+            <div ref={bottomRef}></div>
+
                     
         </div>
+
+        
 
     )
 }
