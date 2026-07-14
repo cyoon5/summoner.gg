9a0cd259bc962d3c36562fb0f0c929d330d6b804
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-07-14 15:47:59 -0700

    Bug fix: duplicate match renders due to async setLoading state

--- components/profile/MatchHistory/MatchHistory.tsx
+++ components/profile/MatchHistory/MatchHistory.tsx
@@ -3,40 +3,45 @@
 import { MatchHistoryProp } from "@/app/types/match";
 import styles from "./MatchHistory.module.css";
 import MatchCard from "../MatchCard/MatchCard";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 
 export default function MatchHistory(props: MatchHistoryProp){
-
 
     const [searchedSummoner, setSearchedSummoner] = useState(props.initialSearchedSummoner)
     const [participantsInMatches, setParticipantsInMatches] = useState(props.initialParticipantsInMatches)
     const [matchInfoList, setMatchInfoList] = useState(props.initialMatchInfoList)
     const [offset, setOffset] = useState(10);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false); //This does not change state immediately, does it after render, hence we need useRef.current
     const [hasMore, setHasMore] = useState(true);
     const bottomRef = useRef(null);
+    const loadingRef = useRef(false);
+
+    const loadMoreMatches = useCallback(async() =>{  //without usecallback, this is created every render(inefficeint)
+
+        if(loadingRef.current || !hasMore)
+            return;
+        loadingRef.current = true;
+        setLoading(true);
+        const res = await fetch(`/api/matches?puuid=${props.puuid}&routing=${props.routing}&start=${offset}&count=10`);
+        const data = await res.json();
+
+        setSearchedSummoner(s => [...s, ...data.searchedSummoner]);
+        setParticipantsInMatches(s => [...s, ...data.participantsInMatches]);
+        setMatchInfoList(s => [...s, ...data.matchInfoList]);
+        
+        setOffset(m => m + 10);
+        loadingRef.current = false;
+        setLoading(false);
+        if(data.searchedSummoner.length < 10)
+            setHasMore(false);
+
+    }, [hasMore, offset])
+
 
     useEffect(() => {
 
         const observer = new IntersectionObserver((entries) => {
-
-            const loadMoreMatches = async() =>{
-                if(loading || !hasMore)
-                    return;
-                setLoading(true);
-                const res = await fetch(`/api/matches?puuid=${props.puuid}&routing=${props.routing}&start=${offset}&count=10`);
-                const data = await res.json();
-
-                setSearchedSummoner(s => [...s, ...data.searchedSummoner]);
-                setParticipantsInMatches(s => [...s, ...data.participantsInMatches]);
-                setMatchInfoList(s => [...s, ...data.matchInfoList]);
-                
-                setOffset(m => m + 10);
-                setLoading(false);
-                if(data.searchedSummoner.length < 10)
-                    setHasMore(false);
-            }
 
             if(entries[0].isIntersecting) {
                 loadMoreMatches();
@@ -49,9 +54,7 @@ export default function MatchHistory(props: MatchHistoryProp){
 
         return () => observer.disconnect(); 
 
-    }, [hasMore, offset]);
-
-
+    },[loadMoreMatches]);
 
 
     return(
@@ -76,6 +79,8 @@ export default function MatchHistory(props: MatchHistoryProp){
 
             </div>
 
+
+            {loading && <div className={styles.loadingSpinner}></div>}
             <div ref={bottomRef}></div>
 
         </div>
