1c573204fdc8d159d261aba46bf9a90faf2f7fc3
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-07-28 15:23:56 -0700

    leaderboard service first runthrough complete

--- app/services/leaderboardService.ts
+++ app/services/leaderboardService.ts
@@ -1,14 +1,109 @@
+import { ApexLeague, ApexLeagueEntry, leaderboardEntry } from "../types/leaderboard";
+import { REGION_MAPPING } from "./constants";
 const api_key = process.env.RIOT_API_KEY;
 
-async function getChallengerLeagues(region: string, queue: string){
+async function getChallengerLeagues(region: string, queue: string): Promise<ApexLeague>{
 
     if(!api_key)
         throw new Error("Missing api key");
     
     const leaderboardURL = `https://${region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/${queue}`;
     const response = await fetch(leaderboardURL, {headers: {"X-Riot-Token": api_key}});
+
+    if(!response.ok)
+        throw new Error("Failed to get challenger queues");
+
+    return await response.json();    
+}
+
+
+async function getGrandmasterLeagues(region: string, queue: string): Promise<ApexLeague>{
+
+    if(!api_key)
+        throw new Error("Missing api key");
+    
+    const leaderboardURL = `https://${region}.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/${queue}`;
+    const response = await fetch(leaderboardURL, {headers: {"X-Riot-Token": api_key}});
+
+    if(!response.ok)
+        throw new Error("Failed to get grandmaster queues");
+
+    return await response.json();    
+}
+
+async function getMasterLeagues(region: string, queue: string): Promise<ApexLeague>{
+
+    if(!api_key)
+        throw new Error("Missing api key");
+    
+    const leaderboardURL = `https://${region}.api.riotgames.com/lol/league/v4/masterleagues/by-queue/${queue}`;
+    const response = await fetch(leaderboardURL, {headers: {"X-Riot-Token": api_key}});
+
+    if(!response.ok)
+        throw new Error("Failed to get master queues");
+
+    return await response.json();    
+}
+
+
+
+async function getLeaderboard(region: string, queue: string, start: number, count: number): Promise<leaderboardEntry[]>{
+
+
+
+    const [
+            challengerLeagues, 
+            grandmasterLeagues, 
+            masterLeagues
+          ] = await Promise.all(
+          [
+            getChallengerLeagues(region, queue), 
+            getGrandmasterLeagues(region, queue), 
+            getMasterLeagues(region, queue)
+          ]);
+
+    const challenger = challengerLeagues.entries.map(p =>({...p, tier: "challenger"}));
+    const grandmaster = grandmasterLeagues.entries.map(p =>({...p, tier: "grandmaster"}))
+    const master = masterLeagues.entries.map(p =>({...p, tier: "master"}))
+    const apexLeagues = [...challenger, ...grandmaster, ...master].sort((a,b) => b.leaguePoints - a.leaguePoints);
+
+    const promises = apexLeagues.slice(start,start + count).map(async (entry: ApexLeagueEntry) =>{
+        return await getAccountInfo(entry.puuid, region)
+    })
+
+    const leaderboardAccounts = await Promise.all(promises);
+
+    //currently only works on first page 
+    const leaderboardEntries = leaderboardAccounts.map((p,i) => {
+        const entry: leaderboardEntry = {
+            puuid: p.puuid,
+            gameName: p.gameName,
+            tagLine: p.tagLine,
+            tier: apexLeagues[i].tier,
+            division: apexLeagues[i].division,
+            leaguePoints: apexLeagues[i].leaguePoints,
+            wins: apexLeagues[i].wins,
+            losses: apexLeagues[i].losses,
+            region: region
+        }
+        return entry;
+    })
+
+    return leaderboardEntries;
+    
+}
+
+async function getAccountInfo(puuid: string, region: string){
+
+    if(!api_key)
+        throw new Error("Missing api key");
+
+    const accountURL = `https://${REGION_MAPPING.get(region)}.api.riotgames.com/riot/account/v1/accounts/by-puuid/${puuid}`;
+    const response = await fetch(accountURL, {headers: {"X-Riot-Token": api_key}});
+
     return await response.json();
 }
 
 
-export {getChallengerLeagues}
+
+export {getChallengerLeagues, getLeaderboard}
---
+++ app/types/leaderboard.ts
@@ -0,0 +1,30 @@
+export type leaderboardEntry={
+    puuid: string;
+    gameName: string;
+    tagLine: string;
+    tier: string;
+    division: string;
+    leaguePoints: number;
+    wins: number;
+    losses: number;
+    region: string;
+}
+
+export type ApexLeagueEntry={
+    puuid: string;
+    leaguePoints: number;
+    tier: string;
+    division: string;
+    wins: number;
+    losses: number;
+    veteran: boolean;
+    inactive: boolean;
+    freshBlood: boolean;
+    hotStreak: boolean;
+}
+
+export type ApexLeague={
+    tier: string;
+    queue: string;
+    entries: ApexLeagueEntry[];
+}
