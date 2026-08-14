f996c3feaeaede668915aa114e9bc9e96a83ecbe
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-08-14 15:35:17 -0700

    Landing page card structure started

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 'use client'
 
+import Image from "next/image"
 import styles from "./page.module.css"
+import mainImage from '../public/home-page-cards/lolchamp.jpg'
+import championCardImage from '../public/home-page-cards/background.webp'
+
 
 export default function Home(){
 
@@ -10,6 +14,40 @@ export default function Home(){
 
     return(
         <div className = {styles.contentContainer}>
+
+            <div className = {styles.homeNav}>
+                <span className = {styles.siteName}> summoner.gg </span>
+            </div>
+
+            <div className = {styles.cardContainer}>
+                
+                <div className = {styles.mainCard}>
+                    <Image
+                        className = {styles.mainImage}
+                        src = {mainImage}
+                        alt = "Nav Card Image"
+                        priority
+                    />
+                </div>
+
+                <div className = {styles.subCardContainer}>
+
+                    <Image
+                        className = {styles.subImage}
+                        src = {mainImage}
+                        alt = "Nav Card Image"
+                        priority
+                    />
+
+                    <Image
+                        className = {styles.subImage}
+                        src = {championCardImage}
+                        alt = "Nav Card Image"
+                        priority
+                    />
+
+                </div>
+            </div>
 
         </div>
     )
