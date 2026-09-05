bdc6966b07ceb8d6c5c04a8fedd3a838ab88ffdc
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-09-05 14:14:38 -0700

    added match persistence operations for psql

--- app/types/repository.ts
+++ app/types/repository.ts
@@ -1,6 +1,57 @@
-export type AccountInsert = {
+export type Account = {
     puuid: string,
     game_name: string,
     tag_line: string,
     platform: string
+}
+
+export type Match = {
+    match_id: string
+    game_mode: string;
+    game_duration: number;
+    match_date: number;
+}
+
+export type Participant = {
+    puuid: string,
+    match_id: string,
+    champion_id: string,
+    kills: number,
+    deaths: number,
+    assists: number,
+    role: string,
+    creep_score: number,
+    damage_dealt: number,
+    gold: number,
+    champion_level: number,
+    vision_score: number,
+    team: string,
+    win: boolean
+}
+
+export type ParticipantRune = {
+    puuid: string,
+    match_id: string,
+    slot_type: 'PRIMARY_KEYSTONE' | 'PRIMARY_SLOT_1' | 'PRIMARY_SLOT_2' | 'PRIMARY_SLOT_3' | 'SECONDARY_SLOT_1' | 'SECONDARY_SLOT_2' | 'OFFENSE' | 'FLEX' | 'DEFENSE',
+    rune_id: number
+}
+
+export type ParticipantItem = {
+    puuid: string,
+    match_id: string,
+    item_id: number
+}
+
+export type ParticipantSpell = {
+    puuid: string,
+    match_id: string,
+    spell_id: number
+}
+
+export type RankSnapshot = {
+    puuid: string,
+    tier: string,
+    division: string,
+    league_points: number,
+    snapshot_date: Date
 }
--- repositories/matchRepository.ts
+++ repositories/matchRepository.ts
@@ -1,19 +1,129 @@
-import { AccountInsert } from "@/app/types/repository";
+import { Account, Match, Participant, ParticipantItem, ParticipantRune, ParticipantSpell, RankSnapshot } from "@/app/types/repository";
 import { Client } from "pg";
 
 //platform i.e. na1
 
-//this does not need to release client since the transaction owner should do it
-export async function insertAccountData(client: Client, account: AccountInsert){
-
+export async function insertAccount(client: Client, account: Account){
     const params = [
         account.puuid, 
         account.game_name, 
         account.tag_line, 
         account.platform
     ];
-    const statement = `INSERT INTO account(puuid, game_name, tag_line, platform)   
-                       VALUES ($1,$2,$3,$4)
-                       ON CONFLICT DO NOTHING`;
+    const statement = `
+        INSERT INTO account(puuid, game_name, tag_line, platform)   
+        VALUES ($1,$2,$3,$4)
+        ON CONFLICT DO NOTHING
+    `;
+    
+    await client.query(statement, params);
+}
+
+export async function insertMatch(client: Client, match: Match){
+    const params = [
+        match.match_id,
+        match.game_mode,
+        match.game_duration,
+        match.match_date
+    ];
+    const statement = `
+        INSERT INTO match(match_id, game_mode, game_duration, match_date)
+        VALUES ($1,$2,$3,$4)
+        ON CONFLICT DO NOTHING
+    `;
+
+    await client.query(statement, params);
+}
+
+export async function insertParticipant(client: Client, participant: Participant){
+    const params = [
+        participant.puuid,
+        participant.match_id,
+        participant.champion_id,
+        participant.kills,
+        participant.deaths,
+        participant.assists,
+        participant.role,
+        participant.creep_score,
+        participant.damage_dealt,
+        participant.gold,
+        participant.champion_level,
+        participant.vision_score,
+        participant.team,
+        participant.win
+    ];
+
+    const statement = `
+        INSERT INTO participant(puuid, match_id, champion_id, kills, deaths, assists, role, creep_score, damage_dealt, gold, champion_level, vision_score, team, win)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+        ON CONFLICT DO NOTHING
+    `;
+
+    await client.query(statement, params);
+}
+
+export async function insertParticipantRune(client: Client, participantRune: ParticipantRune){
+    const params = [
+        participantRune.puuid,
+        participantRune.match_id,
+        participantRune.slot_type,
+        participantRune.rune_id
+    ];
+
+    const statement = `
+        INSERT INTO participantrune(puuid, match_id, slot_type, rune_id)
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT DO NOTHING
+    `;
+
+    await client.query(statement, params);
+}
+
+export async function insertParticipantItem(client: Client, participantItem: ParticipantItem){
+    const params = [
+        participantItem.puuid,
+        participantItem.match_id,
+        participantItem.item_id
+    ];
+
+    const statement = `
+        INSERT INTO participantitem (puuid, match_id, item_id)
+        VALUES ($1, $2, $3)
+        ON CONFLICT DO NOTHING
+    `;
+
+    await client.query(statement, params);
+}
+
+export async function insertParticipantSpell(client: Client , participantSpell: ParticipantSpell){
+    const params = [
+        participantSpell.puuid,
+        participantSpell.match_id,
+        participantSpell.spell_id
+    ];
+
+    const statement = `
+        INSERT INTO participantspell (puuid, match_id, spell_id)
+        VALUES ($1, $2, $3)
+        ON CONFLICT DO NOTHING
+    `;
+
+    await client.query(statement, params);
+}
+
+export async function insertRankSnapshot(client: Client, rankSnapshot: RankSnapshot){
+    const params = [
+        rankSnapshot.puuid,
+        rankSnapshot.tier,
+        rankSnapshot.division,
+        rankSnapshot.league_points,
+        rankSnapshot.snapshot_date
+    ];
+
+    const statement = `
+        INSERT INTO ranksnapshot (puuid, tier, division, league_points, snapshot_date)
+        VALUES ($1, $2, $3, $4, $5)
+    `;
+
     await client.query(statement, params);
 }
